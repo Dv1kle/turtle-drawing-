ff2b504cfa6d31c58ead27ad38567948379763b4
Dv1kle <91840313+Dv1kle@users.noreply.github.com>
2023-02-23 12:54:48 +0200

    Add files via upload

---
+++ turtle drawing.py
@@ -0,0 +1,86 @@
+import turtle as t
+
+sides = 2
+t.bgcolor('black')
+t.pencolor('yellow')
+t.pensize(3)
+t.speed(10)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(30)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(35)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(30)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(35)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(30)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(35)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(30)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(35)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(30)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(35)
+for k in range(1):
+    for i in range(sides):
+        t.forward(100)
+        t.left(60)
+        t.forward(100)
+        t.left(120)   
+    t.left(30)
+
+
